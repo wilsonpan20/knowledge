5bfe9ada588eca9d35cbf766b6369dfbd39097f7
wilsonpan20 <wilson.evangelista22@gmail.com>
2022-12-17 23:15:52 -0300

    Criado a comunicação com mongoDB

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/knowledge_stats', { useNewUrlParser: true })
+mongoose.set("strictQuery", true);
+mongoose.connect('mongodb://localhost:27017/knowledge_stats')
     .catch(e => {
         const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
