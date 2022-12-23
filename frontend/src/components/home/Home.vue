27a22caab2013c2c855b46afa0cf6e50d42ff589
wilsonpan20 <wilson.evangelista22@gmail.com>
2022-12-23 16:47:14 -0300

    Criado componente de Home

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -5,18 +5,55 @@
         main="Dasboard"
         sub="Base de Conhecimento"
         />
+        <div class="stats">
+            <stat 
+            title="categorias" 
+            :value="stat.categories" 
+            icon="fa fa-folder"
+            color="#d54d50"/>
+            <stat title="Artigos" 
+            :value="stat.articles"
+             icon="fa fa-file" 
+             color="#3bc480"/>
+            <stat title="Usuários"
+             :value="stat.users" 
+             icon="fa fa-user" 
+             color="#3282cd"/>
+
+        </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle.vue';
+import Stat from './Stat.vue';
+import axios from 'axios';
+import {baseApiUrl} from '@/config/global'
 export default {
     name:'Home',
-    components:{PageTitle}
+    components:{PageTitle,Stat},
+    data(){
+        return{
+            stat:{}
+        }
+    },
+    methods:{
+        getStat(){
+            axios.get(`${baseApiUrl}/stats`)
+            .then(response => this.stat = response.data)
+        }
+    },
+    mounted(){
+       this.getStat()
+    }
 
 }
 </script>
 
 <style>
-
+.stats{
+    display:flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,10 @@ import './config/bootstrap'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO
+
+require('axios').defaults.headers.common['Authorization'] = 'bearer  eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IldpbHNvbiBFdmFuZ2VsaXN0YSAiLCJlbWFpbCI6IndpbHNvbi5ldmFuZ2VsaXN0YTIyQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzE4MjI4ODcsImV4cCI6MTY3MjA4MjA4N30.Xddk0TqGSlSRJwHyVZ-hnZDEBRSKu2dxyFqgAT5wu-o'
+
 new Vue({
   router,
   store,
