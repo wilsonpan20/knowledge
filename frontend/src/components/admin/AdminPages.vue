daff161124bbe55c6ef9dbd01c3153f7ac47190d
wilsonpan20 <wilson.evangelista22@gmail.com>
2022-12-23 21:15:55 -0300

    Criando pagina Adm

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -5,15 +5,32 @@
         main="Administração do Sistema"
         sub="Cadastro & Cia"
         />
-
+     <div class="admin-pages-tabs">
+        <b-card no-body>
+         <b-tabs card>
+         <b-tab title="Artigos" active>
+         <article-admin />
+         </b-tab>
+          <b-tab title="Categorias">
+            <category-admin/>
+          </b-tab>
+           <b-tab title="Usuários">
+            <user-admin/>
+           </b-tab>
+         </b-tabs>
+        </b-card>
+     </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle.vue';
+import ArticleAdmin from './ArticleAdmin';
+import CategoryAdmin   from './CategoryAdmin';
+import UserAdmin from './UserAdmin'
 export default {
     name:'AdminPages',
-    components:{PageTitle}
+    components:{PageTitle,ArticleAdmin,CategoryAdmin,UserAdmin}
 
 }
 </script>
