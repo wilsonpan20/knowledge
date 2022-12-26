45145ba7d8ff2f2ee45baf49f466ceeb7d300414
wilsonpan20 <wilson.evangelista22@gmail.com>
2022-12-26 20:26:14 -0300

    melhorando paginação

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 
 // TEMPORARIO
 
-require('axios').defaults.headers.common['Authorization'] = 'bearer  eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IldpbHNvbiBFdmFuZ2VsaXN0YSAiLCJlbWFpbCI6IndpbHNvbi5ldmFuZ2VsaXN0YTIyQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzE4MjI4ODcsImV4cCI6MTY3MjA4MjA4N30.Xddk0TqGSlSRJwHyVZ-hnZDEBRSKu2dxyFqgAT5wu-o'
+require('axios').defaults.headers.common['Authorization'] = 'bearer  eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IldpbHNvbiBFdmFuZ2VsaXN0YSAiLCJlbWFpbCI6IndpbHNvbi5ldmFuZ2VsaXN0YTIyQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzIwOTY5MzUsImV4cCI6MTY3MjM1NjEzNX0.mPfUpAmecF0knZyCNVDQVtgWk2eL7M8GiBqdGfw7ImU'
 
 new Vue({
   router,
